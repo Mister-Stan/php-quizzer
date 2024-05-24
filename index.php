ab812b946d96a51608b939431d921afe71133a7b
mister-stan <lucianstan87@yahoo.com>
2024-05-24 17:00:45 +0100

    add layout scaffolding and style

---
+++ index.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>PHP Quizzer</title>
+    <link rel="stylesheet" href="css/style.css" type="text/css" />
+</head>
+<body>
+    <header>
+        <div class="container">
+            <h1>PHP Quizzer</h1>
+        </div>
+    </header>
+    <main>
+        <div class="container">
+        <h2>Test your PHP Knowledge</h2>
+        <p>This is a multiple choice quizz to test your knowledge of PHP</p>
+        <ul>
+            <li><strong>Number of Questions: </strong>5</li>
+            <li><strong>Type: </strong>Multiple Choice</li>
+            <li><strong>Estimated Time: </strong>4 Minutes</li>
+        </ul>
+        <a href="question.php?n=1" class="start">Start Quiz</a>
+        </div>
+    </main>   
+    <footer>
+        <div class="container">
+            Copyright &copy; 2024 PHP Quizzer
+        </div> 
+</body>
+</html>
